6c49ec5d93f7dbb3f2d352327038007b4ab9a306
Anthony-Dv <dvoracekantonin@icloud.com>
2025-08-03 14:50:42 +0200

    fixed

--- data/fetch_data.py
+++ data/fetch_data.py
@@ -22,13 +22,18 @@ class DB_Handler:
         except Error as e:
             print(f"SQLite error: {e}")
 
+    def commit_and_close(self):
+        self.con.commit()
+        self.con.close()
+        print("Successfully commited and closed connection")
+
     def drop_all_tables(self):
         self.cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
         tables = self.cur.fetchall()
         for table in tables:
-            self.cur.execute(f'DROP TABLE IF EXISTS "{table}";')
-            self.con.commit()
-            print(f"Dropping table: {table} from db: {self.db}")
+            table_name = table[0]
+            self.cur.execute(f'DROP TABLE IF EXISTS "{table_name}";')
+            print(f"Dropping table: {table_name} from db: {self.db}")
         print(f"Tables that should have been dropped: {tables}")
 
     def create_tables(self):
@@ -36,23 +41,27 @@ class DB_Handler:
             for ticker in self.tickers:
                 sql = f"""CREATE TABLE IF NOT EXISTS "{ticker}" (
                     Date TEXT, Open REAL, High REAL, Low REAL,
-                    Close REAL, Volume INTEGER, Dividends REAL, Stock_Splits REAL
+                    Close REAL, Volume INTEGER, Dividends REAL
                 );"""
+                self.cur.execute(sql)
                 print(f"Successfully created table: {ticker}")
-            self.con.commit()
         except Error as e:
             print(f"SQLite error: {e}")
 
     def insert_history(self):
         try:
             for ticker in self.tickers:
-                last_date = self._retrieve_last_date_formated(ticker)
                 yf_ticker = yf.Ticker(ticker)
-                df = yf_ticker.history(period="max").reset_index()
+                last_date = self._retrieve_last_date_formated(ticker)
+                if last_date:
+                    df = yf_ticker.history(period="max", start=last_date).reset_index()
+                else:
+                    df = yf_ticker.history(period="max").reset_index()
+
                 df["Date"] = pd.to_datetime(df["Date"])
                 df["Date"] = df["Date"].dt.strftime("%Y-%m-%d")
+                print(df["Date"].head())
                 df.to_sql(ticker, self.con, index=False, if_exists="append")
-                self.con.commit()
                 print(f"Successfully inserted history into table: {ticker}")
 
         except Error as e:
@@ -61,13 +70,18 @@ class DB_Handler:
     def _retrieve_last_date_formated(self, ticker):
         last_date = self.cur.execute(
             f'SELECT Date FROM "{ticker}" ORDER BY Date DESC LIMIT 1;'
-        ).fetchone()[0]
-        last_date_formated = f"{last_date[:4]}-{last_date[4:6]}-{last_date[6:]}"
-        print(last_date_formated)
-        return last_date_formated
+        ).fetchone()
+        if last_date is None:
+            print(f"Table: {ticker} is empty, will fetch all historical data")
+            return None
+        else:
+            last_date = last_date[0]
+            print(f"last_date: {last_date}")
+            return last_date
 
 
 db_handler = DB_Handler(db, tickers)
 db_handler.drop_all_tables()
 db_handler.create_tables()
 db_handler.insert_history()
+db_handler.commit_and_close()
